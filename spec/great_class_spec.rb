3f132b82bb5772a96f9dcb885eb61656ace2c5f7
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-09-21 00:48:43 +0900

    add sample spec to make a fictional pull request

---
+++ spec/great_class_spec.rb
@@ -0,0 +1,23 @@
+describe GreatClass do
+
+  describe 'some_subject_method' do
+    subject { described_class.new }
+
+    before do
+      subject.some_subject_method
+
+      allow(some_other_object).to receive(:some_important_method).and_return 'stubbed_value'
+    end
+
+    it 'makes itself special!' do
+      is_expected.to be_some_special_state 
+    end
+
+    it 'remembers to do the important thing!' do
+      expect(some_other_object).to receive(:some_important_method)
+      subject.some_subject_method
+    end
+
+  end
+
+end
