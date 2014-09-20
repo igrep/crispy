6905c894d31636fe6cfbd80db8703bbc10cf7070
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-09-21 03:08:37 +0900

    actually change by crispy to show the diff

--- spec/great_class_spec.rb
+++ spec/great_class_spec.rb
@@ -2,27 +2,14 @@ describe GreatClass do
   describe 'some_subject_method' do
     subject { described_class.new }
     before do
-      allow(SomeModule).to receive(:some_important_method).and_return 'stubbed_value'
+      spy_into SomeModule
       subject.some_subject_method
     end
     it 'makes itself special!' do
       is_expected.to be_some_special_state 
     end
     it 'remembers to do the important thing!' do
-      expect(SomeModule).to receive(:some_important_method)
-      subject.some_subject_method
+      expect(spy(SomeModule).received(:some_important_method)).to be true
     end
   end
-end
-
-__END__
-before do
-  spy_into SomeModule
-  subject.some_subject_method
-end
-
-...
-
-it 'remembers to do the important thing!' do
-  expect(spy(SomeModule).received(:some_important_method)).to be true
 end
