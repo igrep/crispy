b21bb76277bf1fac1d0ea26636f60c4d75d051ec
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-10-24 08:29:07 +0900

    implement received_with_receiver?, received_once_with_receiver?, and count_received

--- lib/crispy/crispy_internal/class_spy.rb
+++ lib/crispy/crispy_internal/class_spy.rb
@@ -36,6 +36,28 @@ module Crispy
         @received_messages_with_receiver
       end
 
+      COMMON_RECEIVED_MESSAGE_METHODS_DEFINITION.each do|method_name, core_definition|
+        method_name_with_receiver = method_name.sub(/\??\z/) do|question_mark|
+          "_with_receiver#{question_mark}"
+        end
+
+        binding.eval(<<-END, __FILE__, (__LINE__ + 1))
+          def #{method_name_with_receiver} receiver, received_method_name, *received_arguments, &received_block
+            if received_arguments.empty? and received_block.nil?
+              recevier_and_received_method_name = [receiver, received_method_name]
+              received_messages_with_receiver.map do|received_message_with_receiver|
+                [received_message_with_receiver.receiver, received_message_with_receiver.method_name]
+              end.#{sprintf(core_definition, 'recevier_and_received_method_name')}
+            else
+              received_message_with_receiver = ::Crispy::CrispyReceivedMessageWithReceiver.new(
+                receiver, received_method_name, *received_arguments, &received_block
+              )
+              received_messages_with_receiver.#{sprintf(core_definition, 'received_message_with_receiver')}
+            end
+          end
+        END
+      end
+
       def erase_log
         @received_messages_with_receiver.clear
       end
