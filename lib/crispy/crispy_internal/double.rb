199c8740643db7fbc9703068abbcca4084c0ff6a
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2014-12-28 16:50:45 +0900

    delegate stub feature to spy

--- lib/crispy/crispy_internal/double.rb
+++ lib/crispy/crispy_internal/double.rb
@@ -3,23 +3,19 @@ require 'crispy/crispy_internal/with_stubber'
 module Crispy
   module CrispyInternal
     class Double
-      include WithStubber
 
       def initialize name_or_stubs_map = nil, stubs_map = {}
         if name_or_stubs_map.is_a? ::Hash
           @name = ''.freeze
-          initialize_stubber(name_or_stubs_map)
+          @spy = ::Crispy.spy_into(self, name_or_stubs_map)
         else
           @name = name_or_stubs_map
-          initialize_stubber(stubs_map)
+          @spy = ::Crispy.spy_into(self, stubs_map)
         end
-        singleton_class =
-          class << self
-            self
-          end
-        prepend_stubber singleton_class
+      end
 
-        ::Crispy.spy_into self
+      def stub *arguments, &definition
+        @spy.stub(*arguments, &definition)
       end
 
     end
