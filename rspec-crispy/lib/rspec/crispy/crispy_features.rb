feec4644c51b7c2f8936fcc379da8e4cbbf01a79
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2015-01-29 21:36:27 +0900

    rough implementation for failure_messages

--- rspec-crispy/lib/rspec/crispy/crispy_features.rb
+++ rspec-crispy/lib/rspec/crispy/crispy_features.rb
@@ -43,7 +43,9 @@ module RSpec
         end
 
         def matches?(subject)
-          matched_spy?(spy_of_subject subject)
+          @subject = subject
+          @spy_of_subject = spy_of_subject subject
+          matched_spy?(@spy_of_subject)
         end
 
         def matched_spy? spy
@@ -63,9 +65,29 @@ module RSpec
         end
 
         def failure_message
+          result = "Expected #{@subject.inspect} to have received :#@method_name method"
+          result << "with #@arguments" unless @arguments.empty?
+          result << ".\n"
+          result << actually_received_messages_for_failure_message
+          result
         end
 
         def failure_message_when_negated
+          result = "Expected #{@subject.inspect} NOT to have received :#@method_name method"
+          result << "with #@arguments" unless @arguments.empty?
+          result << ". But actually received.\n"
+          result
+        end
+
+        def actually_received_messages_for_failure_message
+          if @spy_of_subject.received_messages.empty?
+            result << "Actually, it has received no messages.\n"
+          else
+            result << "Actually, it has received these messages:\n"
+            @spy_of_subject.received_messages.each do|received_message|
+              result << "  it.#{received_message.method_name}(#{received_message.join(', '.freeze)})\n"
+            end
+          end
         end
 
         def spy_of_subject subject
@@ -82,6 +104,22 @@ module RSpec
           def matched_spy? spy
             spy.received_once? @method_name, *@arguments
           end
+
+          def failure_message
+            result = "Expected #{@subject.inspect} to have received :#@method_name method"
+            result << "with #@arguments" unless @arguments.empty?
+            result << "ONLY ONCE.\n"
+            result << actually_received_messages_for_failure_message
+            result
+          end
+
+          def failure_message_when_negated
+            result = "Expected #{@subject.inspect} to have received :#@method_name method"
+            result << "with #@arguments" unless @arguments.empty?
+            result << "NOT ONCE.\n"
+            result << actually_received_messages_for_failure_message
+            result
+          end
         end
 
         class NTimes < self
@@ -92,7 +130,26 @@ module RSpec
           end
 
           def matched_spy? spy
-            @n == spy.count_received(@method_name, *@arguments)
+            @actual_count = spy.count_received(@method_name, *@arguments)
+            @n == @actual_count
+          end
+
+          def failure_message
+            result = "Expected #{@subject.inspect} to have received :#@method_name method"
+            result << "with #@arguments" unless @arguments.empty?
+            result << "some particular times.\n"
+            result << "  Expected: #@n times.\n"
+            result << "    Actual: #@actual_count times.\n"
+            result << actually_received_messages_for_failure_message
+            result
+          end
+
+          def failure_message_when_negated
+            result = "Expected #{@subject.inspect} to have received :#@method_name method"
+            result << "with #@arguments" unless @arguments.empty?
+            result << "NOT #@n times.\n"
+            result << actually_received_messages_for_failure_message
+            result
           end
 
         end
